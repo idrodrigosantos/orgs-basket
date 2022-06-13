ecb48a4d73620f2a557188e412fce04339565860
Rodrigo Santos <rodrigosantos.git@gmail.com>
2022-06-13 17:27:57 -0300

    Add texts and images

--- src/mocks/basket.js
+++ src/mocks/basket.js
@@ -1,4 +1,9 @@
 import logo from '../../assets/logo.png';
+import tomato from '../../assets/fruits/tomato.png';
+import broccoli from '../../assets/fruits/broccoli.png';
+import potato from '../../assets/fruits/potato.png';
+import cucumber from '../../assets/fruits/cucumber.png';
+import pumpkin from '../../assets/fruits/pumpkin.png';
 
 const basket = {
   top: {
@@ -11,6 +16,32 @@ const basket = {
     description:
       'Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha',
     price: 'R$ 40,00',
+    button: 'Comprar',
+  },
+  items: {
+    title: 'Itens da cesta',
+    list: [
+      {
+        name: 'Tomate',
+        image: tomato,
+      },
+      {
+        name: 'Brócolis',
+        image: broccoli,
+      },
+      {
+        name: 'Batata',
+        image: potato,
+      },
+      {
+        name: 'Pepino',
+        image: cucumber,
+      },
+      {
+        name: 'Abóbora',
+        image: pumpkin,
+      },
+    ],
   },
 };
 
