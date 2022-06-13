9560fabc570925e724310ee0c59264efab53e6e4
Rodrigo Santos <rodrigosantos.git@gmail.com>
2022-06-13 15:32:13 -0300

    Change Basket component folder

---
+++ src/screens/Basket/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Top from './components/Top';
+import Details from './components/Details';
+
+export default function Basket() {
+  return (
+    <>
+      <Top />
+      <View style={styles.basket}>
+        <Details />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  basket: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+});
